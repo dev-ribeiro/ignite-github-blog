1fdcb7603050570d3a15912790b40b3181ba678f
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-11-13 21:53:12 -0300

    styles: import colors of Figma

--- src/styles/themes/defaultTheme.ts
+++ src/styles/themes/defaultTheme.ts
@@ -1,3 +1,16 @@
 export const defaultTheme = {
-  white: '#fff',
-}
+  white: '#E7EDF4',
+
+  black: '#040F1A',
+
+  'gray-100': '#C4D4E3',
+  'gray-200': '#AFC2D4',
+
+  'blue-200': '#7B96B2',
+  'blue-300': '#3A536B',
+  blue: '#3294F8',
+  'blue-600': '#1C2F41',
+  'blue-700': '#112131',
+  'blue-800': '#0B1B2B',
+  'blue-900': '#071422',
+} as const
