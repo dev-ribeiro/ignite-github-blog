996f680444086c9edec8cb5db3d916036d7cdaf1
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-11-14 22:06:20 -0300

    fix: adjust Theme Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/defaultTheme'
 
 export function App() {
   return (
-    <BrowserRouter>
-      <Router />
-      <GlobalStyle />
-    </BrowserRouter>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
