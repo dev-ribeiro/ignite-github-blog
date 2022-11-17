5d4267b352ba86d816e7688502bcf20bbcefff3f
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-11-16 21:24:11 -0300

    fix: adjust Context Provider in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { PostContextProvider } from './contexts/PostsContext'
 import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/defaultTheme'
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <Router />
+    <PostContextProvider>
+      <ThemeProvider theme={defaultTheme}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
         <GlobalStyle />
-      </BrowserRouter>
-    </ThemeProvider>
+      </ThemeProvider>
+    </PostContextProvider>
   )
 }
