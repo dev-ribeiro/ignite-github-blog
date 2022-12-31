30a4924e49adc7c22ddfb7fa343dc00de1c5df60
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-12-31 08:36:16 -0300

    fix: adjust counter of posts in search form component

--- src/pages/Home/components/SearchForm/index.tsx
+++ src/pages/Home/components/SearchForm/index.tsx
@@ -1,11 +1,15 @@
+import { useContext } from 'react'
+import { PostContext } from '../../../../contexts/PostsContext'
 import { PublicationsCountContainer, SearchFormContainer } from './styles'
 
 export function SearchForm() {
+  const { posts } = useContext(PostContext)
+
   return (
     <SearchFormContainer>
       <PublicationsCountContainer>
         <h2>Publicações</h2>
-        <span>6 publicações</span>
+        <span>{posts.length} publicações</span>
       </PublicationsCountContainer>
       <input type="text" placeholder="Buscar conteúdo" />
     </SearchFormContainer>
